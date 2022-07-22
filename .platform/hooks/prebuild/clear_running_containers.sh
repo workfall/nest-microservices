fb6be6d023db5adda91eafeb321722c749218b29
Elon Aseneka <elonsantos63@gmail.com>
2022-07-22 20:10:50 +0300

    change gateway name

--- .platform/hooks/prebuild/clear_running_containers.sh
+++ .platform/hooks/prebuild/clear_running_containers.sh
@@ -9,6 +9,8 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "\"${last_command}\" command completed with exit code $?."' EXIT
 
 # docker kill $(docker ps -q)
-docker ps -q | xargs docker stop
-docker rm -f $(docker ps -a -q)
-docker rmi -f $(docker images -q)
+# docker rm -f $(docker ps -a -q)
+# docker rmi $(docker images -q)
+docker stop gateway acl-service test-service healthcheck-service
+docker rm $(docker ps --filter status=exited -q)
+docker image prune -a -f
